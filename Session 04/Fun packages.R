f96b73bcb8fc36ecefcc8af1307b9dfb1eee771a
Jakob <jakob.rieser@t-online.de>
2018-11-13 10:57:03 +0100

    Added the fun packages of session 4

---
+++ Session 04/Fun packages.R
@@ -0,0 +1,16 @@
+devtools::install_github("gaborcsardi/praise")
+install.packages("praise")
+library(praise)
+praise()
+
+install.packages("ps")
+if(!require(devtools)){install.packages(devtools)}
+devtools::install_github("brooke-watson/BRRR")
+library(BRRR)
+skrrrahh(41)
+skrrrahh("snoop")
+skrrrahh()  
+skrrrahh(60)
+skrrrahh(50)
+skrrrahh(51)
+skrrrahh(25)
