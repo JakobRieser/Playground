73e1f6ca6a1be73a47663ee55d25251ae1b07c74
Jakob <jakob.rieser@t-online.de>
2018-12-04 10:32:43 +0100

    added fun package

---
+++ Session 07/Fun Pckge.R
@@ -0,0 +1,6 @@
+devtools::install_github("wilkelab/cowplot")
+install.packages("colorspace", repos="http://R-Forge.R-Project.org")
+devtools::install_github("clauswilke/colorblindr")
+
+library(colorblindr)
+
